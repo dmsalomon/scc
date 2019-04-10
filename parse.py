19ade9064960b38463f119c86a62d4f0036f3f2b
Dov Salomon <dsalomon@nyu.edu>
2019-04-10 04:52:22 -0400

    I'm dumb: make yacc module=self

--- parse.py
+++ parse.py
@@ -10,7 +10,7 @@ class Pparser:
     tokens = Plexer.tokens
     start = 'prog'
 
-    def p_prog(p):
+    def p_prog(self, p):
         '''
         prog : prog statement
              | prog decl
@@ -18,82 +18,82 @@ class Pparser:
         '''
         p[0] = [*(p[1] or []), p[2]]
 
-    def p_prog_error(p):
+    def p_prog_error(self, p):
         'prog : prog error'
         p[0] = [*(p[1] or []), 'ERROR']
 
-    def p_prog_empty(p):
+    def p_prog_empty(self, p):
         'prog : empty'
 
-    def p_decl_array(p):
+    def p_decl_array(self, p):
         'decl : KW_ARRAY id LBRAK range RBRAK set-expr SEMI'
         p[0] = ('ARRAY', p[2], p[4], p[6])
 
-    def p_range(p):
+    def p_range(self, p):
         'range : expr OP_DOTDOT expr'
         p[0] = ('RANGE', p[1], p[3])
 
-    def p_decl_set_expr(p):
+    def p_decl_set_expr(self, p):
         'set-expr : id ASSIGN expr'
         p[0] = ('ASSIGN', p[1], p[3])
 
-    def p_decl_set_expr_empty(p):
+    def p_decl_set_expr_empty(self, p):
         'set-expr : empty'
 
-    def p_decl_local(p):
+    def p_decl_local(self, p):
         'decl : KW_LOCAL id ASSIGN expr SEMI'
         p[0] = ('LOCAL', p[2], p[4])
 
-    def p_decl_global(p):
+    def p_decl_global(self, p):
         'decl : KW_GLOBAL id ASSIGN expr SEMI'
         p[0] = ('GLOBAL', p[2], p[4])
 
-    def p_func_defn(p):
+    def p_func_defn(self, p):
         'defn : KW_DEFUN id LPAR args RPAR body KW_END KW_DEFUN'
         p[0] = ('DEFUN', p[2], p[4], p[6])
 
-    def p_func_args_id(p):
+    def p_func_args_id(self, p):
         'args : id'
         p[0] = [p[1]]
 
-    def p_func_args_list(p):
+    def p_func_args_list(self, p):
         'args : args OP_COMMA id'
         p[0] = [*p[1], p[3]]
 
-    def p_func_body(p):
+    def p_func_body(self, p):
         '''
         body : body statement
              | body decl
         '''
         p[0] = [*(p[1] or []), p[2]]
 
-    def p_func_body_error(p):
+    def p_func_body_error(self, p):
         'body : body error'
         if p[1]:
             p[0] = [*p[1], 'ERROR']
         else:
             p[0] = ['ERROR']
 
-    def p_func_body_empty(p):
+    def p_func_body_empty(self, p):
         'body : empty'
 
-    def p_statement_assign(p):
+    def p_statement_assign(self, p):
         'statement : lhs ASSIGN expr SEMI'
         p[0] = ('ASSIGN', p[1], p[3])
 
-    def p_statement_exchange(p):
+    def p_statement_exchange(self, p):
         'statement : lhs EXCHANGE lhs SEMI'
         p[0] = ('EXCHANGE', p[1], p[3])
 
-    def p_statement_while(p):
+    def p_statement_while(self, p):
         'statement : KW_WHILE bool-expr KW_DO stmts KW_END KW_WHILE'
         p[0] = ('WHILE', p[2], p[4])
 
-    def p_statement_if(p):
+    def p_statement_if(self, p):
         'statement : KW_IF bool-expr KW_THEN stmts elsif else KW_END KW_IF'
         p[0] = ('IF', p[2], p[4], p[5], p[6])
 
-    def p_elsif_clause(p):
+    def p_elsif_clause(self, p):
         'elsif : KW_ELSIF bool-expr KW_THEN stmts elsif'
         cur = ('ELSIF', p[2], p[4])
         elsif = p[5]
@@ -102,58 +102,58 @@ class Pparser:
         else:
             p[0] = [cur]
 
-    def p_elsif_clause_empty(p):
+    def p_elsif_clause_empty(self, p):
         'elsif : empty'
 
-    def p_else_clause(p):
+    def p_else_clause(self, p):
         'else : KW_ELSE stmts'
         p[0] = ('ELSE', p[2])
 
-    def p_else_clause_empty(p):
+    def p_else_clause_empty(self, p):
         'else : empty'
 
-    def p_statement_for(p):
+    def p_statement_for(self, p):
         'statement : KW_FOREACH id in iterable KW_DO stmts KW_END KW_FOR'
         p[0] = ('FOREACH', p[2], p[4], p[6])
 
-    def p_keywork_in(p):
+    def p_keywork_in(self, p):
         '''
         in : KW_IN
            | ASSIGN
         '''
         p[0] = p[1]
 
-    def p_iterable(p):
+    def p_iterable(self, p):
         '''
         iterable : range
                  | id
         '''
         p[0] = p[1]
 
-    def p_statement_return(p):
+    def p_statement_return(self, p):
         'statement : RETURN expr SEMI'
         p[0] = ('RETURN', p[2])
 
-    def p_statement_print(p):
+    def p_statement_print(self, p):
         'statement : PRINT expr SEMI'
         p[0] = ('PRINT', p[2])
 
-    def p_stmts(p):
+    def p_stmts(self, p):
         'stmts : stmts statement'
         p[0] = [*(p[1] or []), p[2]]
 
-    def p_stmts_error(p):
+    def p_stmts_error(self, p):
         'stmts : stmts error'
         p[0] = [*(p[1] or []), 'ERROR']
 
-    def p_stmts_empty(p):
+    def p_stmts_empty(self, p):
         'stmts : empty'
 
-    def p_bool_expr(p):
+    def p_bool_expr(self, p):
         'bool-expr : expr boolop expr'
         p[0] = (p[2], p[1], p[3])
 
-    def p_bool_op(p):
+    def p_bool_op(self, p):
         '''
         boolop : OP_LESS
                | OP_GREATER
@@ -164,18 +164,18 @@ class Pparser:
         '''
         p[0] = p[1]
 
-    def p_lhs_lvalue(p):
+    def p_lhs_lvalue(self, p):
         'lhs : lvalue'
         p[0] = p[1]
 
-    def p_lhs_list(p):
+    def p_lhs_list(self, p):
         'lhs : lhs OP_COMMA lvalue'
         if isinstance(p[1], list):
             p[0] = p[1] + [p[3]]
         else:
             p[0] = [p[1], p[3]]
 
-    def p_lvalue(p):
+    def p_lvalue(self, p):
         '''
         lvalue : id
                | tuple-ref
@@ -190,14 +190,14 @@ class Pparser:
             ('right', 'CALL'),
     )
 
-    def p_expr_tuple_constructor(p):
+    def p_expr_tuple_constructor(self, p):
         'expr : expr OP_COMMA expr'
         if isinstance(p[1], list):
             p[0] = [*p[1], p[3]]
         else:
             p[0] = [p[1], p[3]]
 
-    def p_expr_binop(p):
+    def p_expr_binop(self, p):
         '''
         expr : expr OP_PLUS  expr
              | expr OP_MINUS expr
@@ -206,47 +206,47 @@ class Pparser:
         '''
         p[0] = (p[2], p[1], p[3])
 
-    def p_expr_par(p):
+    def p_expr_par(self, p):
         'expr : LPAR expr RPAR'
         p[0] = p[2]
 
-    def p_expr_int(p):
+    def p_expr_int(self, p):
         'expr : int'
         p[0] = p[1]
 
-    def p_expr_id(p):
+    def p_expr_id(self, p):
         'expr : id'
         p[0] = p[1]
 
-    def p_expr_func_call(p):
+    def p_expr_func_call(self, p):
         'expr : id expr %prec CALL'
         p[0] = ('CALL', p[1], p[2])
 
-    def p_expr_tuple_ref(p):
+    def p_expr_tuple_ref(self, p):
         'expr : tuple-ref'
         p[0] = p[1]
 
-    def p_expr_array_index(p):
+    def p_expr_array_index(self, p):
         'expr : array-index'
         p[0] = p[1]
 
-    def p_tuple_ref(p):
+    def p_tuple_ref(self, p):
         'tuple-ref : id OP_DOT int'
         p[0] = ('TREF', p[1], p[3])
 
-    def p_array_index(p):
+    def p_array_index(self, p):
         'array-index : id LBRAK expr RBRAK'
         p[0] = ('INDEX', p[1], p[3])
 
-    def p_int(p):
+    def p_int(self, p):
         'int : INT_LIT'
         p[0] = ('INT', p[1])
 
-    def p_id(p):
+    def p_id(self, p):
         'id : ID'
         p[0] = ('ID', p[1])
 
-    def p_empty(p):
+    def p_empty(self, p):
         'empty : '
         pass
 
@@ -266,11 +266,8 @@ class Pparser:
         self.src = src
         self.err = False
 
-        errorfunc = type(self).p_error
-        type(self).p_error = types.MethodType(type(self).p_error, self)
         self.lexer = Plexer(src)
-        self.parser = yacc.yacc(module=type(self))
-        type(self).p_error = errorfunc
+        self.parser = yacc.yacc(module=self)
 
     def parse(self, **kw):
         self.err = False
